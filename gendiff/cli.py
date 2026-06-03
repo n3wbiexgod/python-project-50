9713b0f928a58ac08e4b0bf46e27eb68f31db8ee
n3wbiexgod <greeddyman@gmail.com>
2026-06-03 19:14:59 +0400

    Add gendiff CLI with argparse

---
+++ gendiff/cli.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.',
+    )
+
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    return parser.parse_args()
