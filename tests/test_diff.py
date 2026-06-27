da9d70f158a8205838fd2f7f383671d6b2ddb108
n3wbiexgod <greeddyman@gmail.com>
2026-06-28 03:25:00 +0400

    test: add CLI coverage

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,10 +1,13 @@
+import sys
+
+from gendiff.cli import main
 from gendiff.diff_builder import generate_diff
 
 
 def test_json():
     result = generate_diff(
         "tests/test_data/file1.json",
-        "tests/test_data/file2.json"
+        "tests/test_data/file2.json",
     )
     assert isinstance(result, str)
 
@@ -12,7 +15,7 @@ def test_json():
 def test_yaml():
     result = generate_diff(
         "tests/test_data/file1.yml",
-        "tests/test_data/file2.yml"
+        "tests/test_data/file2.yml",
     )
     assert isinstance(result, str)
 
@@ -21,7 +24,7 @@ def test_plain_format():
     result = generate_diff(
         "tests/test_data/file1.json",
         "tests/test_data/file2.json",
-        "plain"
+        "plain",
     )
 
     assert isinstance(result, str)
@@ -32,8 +35,25 @@ def test_json_format():
     result = generate_diff(
         "tests/test_data/file1.json",
         "tests/test_data/file2.json",
-        "json"
+        "json",
     )
 
     assert isinstance(result, str)
     assert "common" in result or "host" in result
+
+
+def test_cli(capsys, monkeypatch):
+    monkeypatch.setattr(
+        sys,
+        "argv",
+        [
+            "gendiff",
+            "tests/test_data/file1.json",
+            "tests/test_data/file2.json",
+        ],
+    )
+
+    main()
+
+    output = capsys.readouterr().out
+    assert output
